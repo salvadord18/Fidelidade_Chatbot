7da9f1c34959e3ca4a396f6719ae403ee74e8d77
MGN19 <126704147+MGN19@users.noreply.github.com>
2025-05-13 16:29:06 +0100

    n

--- util.py
+++ util.py
@@ -1,9 +1,3 @@
-## Antonio
-#api_key1 = "20211595:29e2994c-2337-48d4-a828-4a8f424edfd0"
-#endpoint = "https://iaapi.novaims.unl.pt/v1/completions"
-
-#key = "8J6pTdfaGgA5r193UVLsBshUspqwNpal42Jse1aHaok1cWNTLpRkJQQJ99BDACYeBjFXJ3w3AAABACOGLa23"
-
 # [i]                                                                                            #
 # [i] Libraries                                                                                   #
 # [i]                                                                                            #
